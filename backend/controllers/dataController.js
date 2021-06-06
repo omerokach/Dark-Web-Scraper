210fe4a8e89c75a49cf931be025fa1de3c23159c
omer rokach <77382841+omerokach@users.noreply.github.com>
2021-06-06 16:26:34 +0300

    data controller

--- backend/controllers/dataController.js
+++ backend/controllers/dataController.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const cheerio = require("cheerio");
 
 module.exports.dataGet = async (req, res) => {
   try {
@@ -8,8 +9,36 @@ module.exports.dataGet = async (req, res) => {
         port: 8118,
       },
     });
-    console.log(response);
-    res.send(response.data);
+    const totalDataArray = [];
+    const $ = cheerio.load(response.data);
+    $(".col-sm-5").each((i, element) => {
+      let string = JSON.stringify($(element).text());
+      console.log(string);
+      let newstr = string.replace(/\\t|\\n|"/g, (match) => {
+        return "";
+      });
+      totalDataArray.push({ title: newstr });
+    });
+    $("ol").each((i, element) => {
+      totalDataArray[i].body = $(element)
+        .children()
+        .text()
+        .replace(/\s\s+/g, " ");
+    });
+    $("div[class=col-sm-6]:not(.text-right)").each((i, element) => {
+      let elementText = JSON.stringify($(element).text()).replace(
+        /\\t|\\n/g,
+        (match) => {
+          return "";
+        }
+      );
+      const by = elementText.match(/(?<=(by ))(.*?)(?= (at))/g);
+      const at = elementText.match(/(?<=(at ))(.*?)(?=")/g);
+      totalDataArray[i].author = by[0];
+      totalDataArray[i].date = at[0];
+    });
+    console.log(totalDataArray);
+    res.send(totalDataArray);
   } catch (error) {
     console.log(error);
     res.send(error);
